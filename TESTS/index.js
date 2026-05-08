9c2664253e1890e081ad34d7da22ea115ae23fb0
bryanrafaelbueno <brthy40@gmail.com>
2026-05-08 10:51:25 -0300

    fix: remove tests from the global repository

--- TESTS/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-async function getAllPokemons(){
-    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=99999&offset=0')
-    return data
-}
-(async () => {
-    const response = await getAllPokemons()
-    console.log(response['results'])
-})()
